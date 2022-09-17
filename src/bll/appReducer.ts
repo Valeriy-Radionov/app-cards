64ef7b4174157af622e6ad6027b9f64c80d8c240
egory4fash <egormaksul@gmail.com>
2022-09-17 19:03:00 +0300

    appReducer logic added preLoader(empty) added

--- src/bll/appReducer.ts
+++ src/bll/appReducer.ts
@@ -1,10 +1,41 @@
-const initialState = {}
+import { AppThunk} from "./store";
+import {Dispatch} from "redux";
+
+
+
+const initialState = {
+    isInitialized: false
+}
 
 export const appReducer = (state = initialState, action: AppActionType): typeof initialState => {
     switch (action.type) {
+        case "APP-INITIALIZED": {
+            return {...state, isInitialized: action.payload.isInitialized}
+        }
         default:
             return state
     }
+
 }
 
-export type AppActionType = any
+export type AppActionType = AppinitializedACType
+
+
+type AppinitializedACType = ReturnType<typeof appInitializedAC>
+
+export const appInitializedAC = (isInitialized: boolean) => {
+    return {
+        type: "APP-INITIALIZED",
+        payload: {
+            isInitialized
+        }
+    } as const
+}
+
+export const initializeAPPThunkCreator = ():AppThunk => {
+    return (dispatch:Dispatch) => {
+        dispatch(appInitializedAC(true))
+    }
+}
+
+
