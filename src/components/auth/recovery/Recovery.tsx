81ce3a04c9b0a14ce395165236fbf5e853e4ffb6
Kas-Telo <alexkas2511@gmail.com>
2022-09-20 00:34:12 +0300

    added validate for email-input

--- src/components/auth/recovery/Recovery.tsx
+++ src/components/auth/recovery/Recovery.tsx
@@ -29,9 +29,13 @@ export const Recovery = () => {
         validate: values => {
             const errors: FormikErrorType = {}
             if (!values.email) {
-                errors.email = 'Email is required'
+                return {
+                    email: 'Email is required'
+                }
             } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-                errors.email = 'Invalid email address';
+                return {
+                    email: 'Invalid email address'
+                }
             }
         }
 
@@ -53,7 +57,6 @@ export const Recovery = () => {
                         </div>
                         <SuperInputText {...formik.getFieldProps('email')}/>
                         <div>{formik.errors.email ? formik.errors.email : null}</div>
-                        {/*<input type="text" value={value} onChange={(e) => setValue(e.currentTarget.value)}/>*/}
                         <SuperButton type={'submit'}>Send</SuperButton>
                     </form>
                     : <EmailSentPage/>
