3919c01176ba513cd4b9ae2902eb2833e0eaf5b5
Kas-Telo <alexkas2511@gmail.com>
2022-09-23 19:47:50 +0300

    deleted unnecessary links

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -16,15 +16,15 @@ export const Header = () => {
 
     return (
         <div className={style.container}>
-            <NavLink to={PATH.LOGIN} className={style.linkNavBar}>LOGIN</NavLink>
-            <NavLink to={PATH.REGISTR} className={style.linkNavBar}>REGISTRATION</NavLink>
-            <NavLink to={PATH.SET_PASSWORD} className={style.linkNavBar}>SET PASSWORD</NavLink>
-            <NavLink to={PATH.RECOVERY} className={style.linkNavBar}>RECOVERY PASSWORD</NavLink>
+            {/*<NavLink to={PATH.LOGIN} className={style.linkNavBar}>LOGIN</NavLink>*/}
+            {/*<NavLink to={PATH.REGISTR} className={style.linkNavBar}>REGISTRATION</NavLink>*/}
+            {/*<NavLink to={PATH.SET_PASSWORD} className={style.linkNavBar}>SET PASSWORD</NavLink>*/}
+            {/*<NavLink to={PATH.RECOVERY} className={style.linkNavBar}>RECOVERY PASSWORD</NavLink>*/}
             <NavLink to={PATH.PROFILE} className={style.linkNavBar}>PROFILE</NavLink>
-            <NavLink to={PATH.TEST} className={style.linkNavBar}>ALL COMPONENTS</NavLink>
-            <NavLink to={PATH.ERROR} className={style.linkNavBar}>ERROR 404 NOT FOUND</NavLink>
-            <SuperButton onClick={logoutHandler}>Logout︎</SuperButton>
-            <SuperButton>Menu︎</SuperButton>
+            {/*<NavLink to={PATH.TEST} className={style.linkNavBar}>ALL COMPONENTS</NavLink>*/}
+            {/*<NavLink to={PATH.ERROR} className={style.linkNavBar}>ERROR 404 NOT FOUND</NavLink>*/}
+            {/*<SuperButton onClick={logoutHandler}>Logout︎</SuperButton>*/}
+            {/*<SuperButton>Menu︎</SuperButton>*/}
         </div>
     );
 };
