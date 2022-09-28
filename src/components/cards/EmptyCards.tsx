c422f1b16e383614c37188a9e6f439226aa7b2e9
NikolyUrsegov <ursegovnikolaj@gmail.com>
2022-09-28 21:37:31 +0300

    create Empty component

--- src/components/cards/EmptyCards.tsx
+++ src/components/cards/EmptyCards.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import s from './Empty.module.scss'
 
-export const EmptyCards = () => {
+type EmptyCardsPropsType = {
+    addNewItem: () => void
+    isMy: boolean
+}
+
+export const EmptyCards: React.FC<EmptyCardsPropsType> = ({addNewItem, isMy}) => {
     return (
-        <div>
-            <div>name</div>
-            <div>
-                <span>There are no cards in this pack that satisfy the search</span>
-            </div>
+        <div className={s.block}>
+            <span>There are no cards in this pack that satisfy the search</span>
+            {isMy
+                ? <button onClick={addNewItem} className={s.addCard}>Add new card</button>
+                : null
+            }
+
         </div>
     );
 };
