00b460285fd092721844c7107e3ccbd04c5718ef
valera <traberker901@gmail.com>
2022-09-29 21:22:29 +0300

    debug: hover cell

--- src/components/packs/TableBody/PacksTableBody.tsx
+++ src/components/packs/TableBody/PacksTableBody.tsx
@@ -3,7 +3,6 @@ import {TableBody, TableCell, TableRow} from '@mui/material'
 import {formatDate} from "../../../utils/formatDate-utils";
 import {CardPackType} from "../../../api/packs/packs-api";
 import {ActionsPacks} from "./ActionsPacks";
-import {useAppSelector} from "../../../bll/store";
 
 type MapTableBodyPropsType = {
     items: CardPackType[]
@@ -18,35 +17,35 @@ export const PacksTableBody: React.FC<MapTableBodyPropsType> = ({
                                                                     updatePack,
                                                                     learnPack
                                                                 }) => {
-    const userID = useAppSelector(state => state.profile.user?._id)
 
-    return (<>
-            <TableBody>
-                {items.map(item => {
-                    return (
-                        <TableRow key={item._id} hover role="complementary">
-                            <TableCell align="left" component="th" scope="row">
-                                {item.name}
-                            </TableCell>
-                            <TableCell align="left">
-                                {item.cardsCount}
-                            </TableCell>
-                            <TableCell align="left">
-                                {formatDate(item.updated)}
-                            </TableCell>
-                            <TableCell align="left">{item.user_name}</TableCell>
-                            <TableCell align="left">
-                                <ActionsPacks learnItem={learnPack}
-                                              updateItem={updatePack}
-                                              deleteItem={deletePack}
-                                              packId={item._id}
-                                              userId={item.user_id}
-                                />
-                            </TableCell>
-                        </TableRow>)
-                })}
-            </TableBody>
-        </>
+    return (
+        <TableBody>
+            {items.map(item => {
+                return (
+                    <TableRow key={item._id} sx={{
+                        "&:hover": {bgcolor: 'lightgray'}
+                    }}>
+                        <TableCell align="left">
+                            {item.name}
+                        </TableCell>
+                        <TableCell align="left">
+                            {item.cardsCount}
+                        </TableCell>
+                        <TableCell align="left">
+                            {formatDate(item.updated)}
+                        </TableCell>
+                        <TableCell align="left">{item.user_name}</TableCell>
+                        <TableCell align="left">
+                            <ActionsPacks learnItem={learnPack}
+                                          updateItem={updatePack}
+                                          deleteItem={deletePack}
+                                          packId={item._id}
+                                          userId={item.user_id}
+                            />
+                        </TableCell>
+                    </TableRow>)
+            })}
+        </TableBody>
     );
 };
 
