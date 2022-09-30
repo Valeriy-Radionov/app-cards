1f607dd411d339874a06a7667aede9e3d999b453
NikolyUrsegov <ursegovnikolaj@gmail.com>
2022-09-30 13:12:46 +0300

    add onClick on zeroing the search parameters

--- src/common/Link/LinkArrow.tsx
+++ src/common/Link/LinkArrow.tsx
@@ -6,12 +6,13 @@ type LinkArrowPropsType = {
     className: string
     to: string
     name: string
+    callback?: () => void
 }
 
-export const LinkArrow: React.FC<LinkArrowPropsType> = ({className, to, name}) => {
+export const LinkArrow: React.FC<LinkArrowPropsType> = ({className, to, name,callback}) => {
     return (
         <div className={className}>
-            <NavLink to={to}>
+            <NavLink to={to} onClick={callback}>
                 <img src={arrow} alt=""/>
                 {name}
             </NavLink>
